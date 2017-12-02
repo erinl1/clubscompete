2815e28e8adde95a66b3ff4bab31dcfc39b80aed
erinl1 <erin.lee.ny@gmail.com>
2017-12-02 14:47:27 -0500

    Committing edits

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :students
   resources :teams
   resources :schools
+  root to: 'home#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
